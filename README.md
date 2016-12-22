4629bfd651564b8bdee42b0226731df8dc4142d0
Prajwal Kumar <prajwalkumar@Prajwals-MacBook-Pro.local>
2016-12-22 23:56:00 +0530

    Fix: added README.md + fixed validator check errors

---
+++ README.md
@@ -0,0 +1,17 @@
+# Neighbourhood-Map
+A project using Google Map API which fetches interesting places in and around gujarat.
+
+
+###How to run?
+Run `index.html` through your web browser or use the github pages link [Click Here](https://prajwalkumarshettigar.github.io/Neighbourhood-Map/)
+
+###Files:
+* `index.html` - Main page
+* css
+  * `style.css` - Takes care of the web-page style.
+  * `sidebar.css` - Takes care of collapsable side navigation bar.
+  * `bootstrap.min.css` - A well known css library used for styling and responsive design.
+* js
+  * `app.js` - uses a MVVM approach to binding Wikipedia data to the DOM.
+  * `map.js` - fetches and computes map related work from Google Map API.
+  * `global.js` - contains globally declared variables.
--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 var map,placeInfoWindow;
       function initMap() {
-        var styles =[{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
+        var styles =[{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}];
 
         // Map Constructor
         map = new google.maps.Map(document.getElementById('map'), {
@@ -33,7 +33,7 @@ function textSearchPlaces(name) {
             }
         });
     }
-//create markers for the fetched places--Note:Parts of the code taken udacity Google Maps API lesson resources.
+//create markers for the fetched places--Note:code patches taken udacity Google Maps API lesson resources.
 function createMarkersForPlaces(places) {
         var bounds = new google.maps.LatLngBounds();
         for (var i = 0; i < places.length; i++) {

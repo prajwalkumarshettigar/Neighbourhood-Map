4c0d5efa98216e145858cbc2e0d166b5a7cd2f86
Prajwal Kumar <prajwalkumar@Prajwals-MacBook-Pro.local>
2016-12-24 11:03:31 +0530

    Update: Added error handling for map, marker click event

--- js/app.js
+++ js/app.js
@@ -1,87 +1,86 @@
 var wikiSuccess = "Know from Wiki:"
 var wikiFail = "No Wikipedia resources about this place"
-var initMark =[];
+var initMark = [];
 viewModel = {
-        wikiName: ko.observableArray(),
-        wikiLinks: ko.observableArray(),
-        wikiInfo:ko.observableArray(),
-        wikiFailed:ko.observable(),
-        wikiHeader:ko.observable(),
-        q:ko.observable(''),
-        markerList: ko.observableArray(),
-        //initial Marker load function
-        init: function(){
-            initMark = markerName.slice();
-            if(!viewModel.q())
+    wikiName: ko.observableArray(),
+    wikiLinks: ko.observableArray(),
+    wikiInfo: ko.observableArray(),
+    wikiFailed: ko.observable(),
+    wikiHeader: ko.observable(),
+    q: ko.observable(''),
+    markerList: ko.observableArray(),
+    //initial Marker load function
+    init: function () {
+        initMark = markerName.slice();
+        if (!viewModel.q())
             viewModel.markerList(initMark);
-        },
-        //click event function for list items
-        listClick : function(data, event) {
-            var name = event.target.innerHTML;
-            var id;//get the id of marker
-            for(var i=0;i<15;i++){
-                if (name === markerName[i]){
-                    id = i;break;
-                }
+    },
+    //click event function for list items
+    listClick: function (data, event) {
+        var name = event.target.innerHTML;
+        var id;//get the id of marker
+        for (var i = 0; i < 15; i++) {
+            if (name === markerName[i]) {
+                id = i; break;
             }
-            viewModel.displayInfo(id);
-            viewModel.wikiAPI(name);
-            },
-
-            displayInfo: function(id){
-                displayInfoWindow(id);
-            },
-        search: function(value) {
-            //live search with Knockout js, Source: stackoverflow
-                viewModel.markerList.removeAll();
-                for(var i=0;i<markerName.length;i++) {
-                if(markerName[i].toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        }
+        viewModel.displayInfo(id);
+        viewModel.wikiAPI(name);
+    },
+    displayInfo: function (id) {
+        displayInfoWindow(id);
+    },
+    search: function (value) {
+        //live search with Knockout js, Source: code patch from stackoverflow
+        viewModel.markerList.removeAll();
+        for (var i = 0; i < placeMarkers.length; i++) {
+            if (markerName[i].toLowerCase().indexOf(value.toLowerCase()) >= 0) {
                 viewModel.markerList.push(markerName[i]);
                 placeMarkers[i].setVisible(true);
-              }
-              else{
+            }
+            else {
                 placeMarkers[i].setVisible(false);
-              }
             }
-          },
-          //wikipedia API request function
-            wikiAPI: function(data){
-                var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + data + "&format=json&callback=wikiCallback";
-                var wikiRequestTimeout = setTimeout(function () {
-                    alert("TimeOut!! Couldn't fetch resources from wikipedia");
-                    }, 8000);
-                $.ajax({
-                    url: wikiUrl,
-                    dataType: "jsonp",
-                    success: function (response) {
-                        var articleName = response[1];
-                        var articleList = response[2];
-                        var articleUrl = response[3];
-                        console.log(articleName);
-                        console.log(articleList);
-                        console.log(articleUrl);
+        }
+    },
+    //wikipedia API request function
+    wikiAPI: function (data) {
+        var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + data + "&format=json&callback=wikiCallback";
+        var wikiRequestTimeout = setTimeout(function () {
+            alert("TimeOut!! Couldn't fetch resources from wikipedia");
+        }, 8000);
+        $.ajax({
+            url: wikiUrl,
+            dataType: "jsonp",
+            success: function (response) {
+                var articleName = response[1];
+                var articleList = response[2];
+                var articleUrl = response[3];
+                console.log(articleName);
+                console.log(articleList);
+                console.log(articleUrl);
 
-                         viewModel.fetchWikiData(articleList, articleUrl, articleName);
-                        clearTimeout(wikiRequestTimeout);
-                    }
-                    });
-            },
-            //assign wiki response data to ko observales
-            fetchWikiData: function(articleList, articleUrl, articleName){
-                if(articleName.length === 0){
-                    viewModel.wikiHeader(wikiFail);
-                    viewModel.wikiName.removeAll();
-                    viewModel.wikiInfo.removeAll();
-                    viewModel.wikiLinks.removeAll();
-                }
-                else{
-                    viewModel.wikiHeader(wikiSuccess);
-                    viewModel.wikiName(articleName);
-                    viewModel.wikiInfo(articleList);
-                    viewModel.wikiLinks(articleUrl);
-                    console.log(viewModel.wikiLinks);
-                }
+                viewModel.fetchWikiData(articleList, articleUrl, articleName);
+                clearTimeout(wikiRequestTimeout);
             }
+        });
+    },
+    //assign wiki response data to ko observales
+    fetchWikiData: function (articleList, articleUrl, articleName) {
+        if (articleName.length === 0) {
+            viewModel.wikiHeader(wikiFail);
+            viewModel.wikiName.removeAll();
+            viewModel.wikiInfo.removeAll();
+            viewModel.wikiLinks.removeAll();
+        }
+        else {
+            viewModel.wikiHeader(wikiSuccess);
+            viewModel.wikiName(articleName);
+            viewModel.wikiInfo(articleList);
+            viewModel.wikiLinks(articleUrl);
+            console.log(viewModel.wikiLinks);
+        }
+    }
 
 }
 viewModel.q.subscribe(viewModel.search);
--- js/map.js
+++ js/map.js
@@ -1,89 +1,91 @@
 var map,placeInfoWindow;
-      function initMap() {
-        var styles =[{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}];
-
-        // Map Constructor
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 22.2587, lng: 71.1924},
-          zoom: 13,
-          styles: styles
-        });
-         textSearchPlaces(locName);
-
-      }
+function initMap() {
+    var styles =[{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}];
+    // Map Constructor
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 22.2587, lng: 71.1924},
+        zoom: 13,
+        styles: styles
+    });
+    textSearchPlaces(locName);
+    };
+//Function for map error handling
+mapError = () => {
+    alert("Sorry we couldn't load the Map, Please hit Refresh or try after sometime")
+}
 //using google query request to fetch for some interesting places
 function textSearchPlaces(name) {
-        var bounds = map.getBounds();
-        var Query = "point of interest"+name ;
-        var placesService = new google.maps.places.PlacesService(map);
-        placesService.textSearch({
-        query: Query,
-        bounds: bounds
-        }, function(results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-                 for (var i = 0; i < 15; i++) {
-                    markerName[i] = results[i].name;
-                 }
-                viewModel.init();
-                createMarkersForPlaces(results);
+    var bounds = map.getBounds();
+    var query = "point of interest"+name ;
+    var placesService = new google.maps.places.PlacesService(map);
+    placesService.textSearch({
+      query: query,
+      bounds: bounds
+    },
+    function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < 15; i++) {
+                markerName[i] = results[i].name;
             }
-            else{
-               window.alert("We couldn't place Markers,Sorry for the inconvinence caused"+
-                            " Please Refresh the page or load after sometime");
-            }
-        });
-    }
+            viewModel.init();
+            createMarkersForPlaces(results);
+        }
+        else{
+            window.alert("We couldn't place Markers,Sorry for the inconvinence caused, Please Refresh the page or load after sometime");
+        }
+    });
+};
 //create markers for the fetched places--Note:code patches taken udacity Google Maps API lesson resources.
 function createMarkersForPlaces(places) {
-        var bounds = new google.maps.LatLngBounds();
-        for (var i = 0; i < places.length; i++) {
-          var place = places[i];
-          var icon = {
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < places.length; i++) {
+        var place = places[i];
+        var icon = {
             url: place.icon,
             size: new google.maps.Size(35, 35),
             origin: new google.maps.Point(0, 0),
             anchor: new google.maps.Point(15, 34),
             scaledSize: new google.maps.Size(25, 25)
-          };
-
+        };
           // Create a marker for each place.
-          var marker = new google.maps.Marker({
+        var marker = new google.maps.Marker({
             map: map,
             icon: icon,
             title: place.name,
             position: place.geometry.location,
             animation: google.maps.Animation.DROP,
             id: place.place_id
-          });
-          placeInfoWindow = new google.maps.InfoWindow();
-          marker.addListener('click', function() {
-              getPlacesDetails(this, placeInfoWindow);
-          });
-          placeMarkers.push(marker);
-          if (place.geometry.viewport) {
+        });
+        placeInfoWindow = new google.maps.InfoWindow();
+        marker.addListener('click', function() {
+            getPlacesDetails(this, placeInfoWindow);
+            viewModel.wikiAPI(this.title);//when click on marker call wikiAPI function defined in 'app.js'.
+        });
+        placeMarkers.push(marker);
+        if (place.geometry.viewport) {
             bounds.union(place.geometry.viewport);
-          } else {
+        } else {
             bounds.extend(place.geometry.location);
-          }
         }
-        map.fitBounds(bounds);
-      }
-
-      function displayInfoWindow(id){
-        console.log(id);
-        getPlacesDetails(placeMarkers[id],placeInfoWindow);
     }
-    //fetch place Details
-      function getPlacesDetails(marker, infowindow) {
+    map.fitBounds(bounds);
+}
+function displayInfoWindow(id){
+    console.log(id);
+    getPlacesDetails(placeMarkers[id],placeInfoWindow);
+}
+//fetch place details to be displayed on the info window--code patches taken from udacity Google API class resources
+function getPlacesDetails(marker, infowindow) {
     marker.setAnimation(google.maps.Animation.BOUNCE);
     setTimeout(function () {
-                marker.setAnimation(null);
-            }, 2000);
+        marker.setAnimation(null);
+    }, 2000);
     var service = new google.maps.places.PlacesService(map);
     var innerHTML;
     service.getDetails({
         placeId: marker.id
-    }, function(place, status) {
+    },
+    function(place, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             infowindow.marker = marker;
             innerHTML = '<div class="infowindow">';
@@ -98,13 +100,13 @@ function createMarkersForPlaces(places) {
             }
             if (place.opening_hours) {
                 innerHTML += '<br><br><strong>Hours:</strong><br>' +
-                    place.opening_hours.weekday_text[0] + '<br>' +
-                    place.opening_hours.weekday_text[1] + '<br>' +
-                    place.opening_hours.weekday_text[2] + '<br>' +
-                    place.opening_hours.weekday_text[3] + '<br>' +
-                    place.opening_hours.weekday_text[4] + '<br>' +
-                    place.opening_hours.weekday_text[5] + '<br>' +
-                    place.opening_hours.weekday_text[6];
+                place.opening_hours.weekday_text[0] + '<br>' +
+                place.opening_hours.weekday_text[1] + '<br>' +
+                place.opening_hours.weekday_text[2] + '<br>' +
+                place.opening_hours.weekday_text[3] + '<br>' +
+                place.opening_hours.weekday_text[4] + '<br>' +
+                place.opening_hours.weekday_text[5] + '<br>' +
+                place.opening_hours.weekday_text[6];
             }
             if (place.photos) {
                 innerHTML += '<br><br><img src="' + place.photos[0].getUrl({
@@ -115,23 +117,20 @@ function createMarkersForPlaces(places) {
             innerHTML += '</div></div>';
             infowindow.setContent(innerHTML);
             infowindow.open(map, marker);
-
-            // Make sure the marker property is cleared if the infowindow is closed.
             infowindow.addListener('closeclick', function() {
                 marker.setAnimation(null);
                 infowindow.marker = null;
             });
         }
         else {
-                //error message
                 infowindow.marker = marker;
                 innerHTML = '<div class="infowindow"><div class="info"><p>Sorry we could not find more Info:</p></div></div>';
                 infowindow.setContent(innerHTML);
                 infowindow.open(map, marker);
                 infowindow.addListener('closeclick', function() {
-                marker.setAnimation(null);
-                infowindow.marker = null;
+                    marker.setAnimation(null);
+                    infowindow.marker = null;
                 });
-            }
-        });
-    }
+        }
+    });
+}
